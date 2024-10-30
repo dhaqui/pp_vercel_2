d3e4fd5109886570ff6e8a69c2bb0583a0d9c6b3
dhaqui <49834807+dhaqui@users.noreply.github.com>
2024-10-30 16:37:59 +0900

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -2,8 +2,10 @@ const paypal = require('@paypal/checkout-server-sdk');
 
 // PayPalの環境設定
 const environment = new paypal.core.SandboxEnvironment(
-  'AV3P5knBPhPGwS7EDayvKulircVel0n9adwODe6qRp1-0vp2pZ2DRaOa0kegvAwQYlJ4K_C78v3RduCG',  // あなたのPayPal Client ID
-  'EATQXaVnsOagF0rvsfzZwaSmk_040HeX1t2aOrLjOOjx2LlCGVCjdJwaQUJ_9rCpI4vYCFDaM2sn3fPb'   // あなたのPayPal Client Secret
+  'AfOmRjUNBmq1frbqWWE6FzeOMHOh1Z7MYtCWowq6wLWbAGHsZTkCesmHCoQkrtxby5THkTfOdTH-LLus',
+  'ENGPKCudH3rO0rVT9m9GVSEmne_lu8IDRWajsVlkdiBO1uobViJeWSglrx9gOQse7n1izwkaV2HE4gKi'
+ // 'AV3P5knBPhPGwS7EDayvKulircVel0n9adwODe6qRp1-0vp2pZ2DRaOa0kegvAwQYlJ4K_C78v3RduCG',  // あなたのPayPal Client ID
+ // 'EATQXaVnsOagF0rvsfzZwaSmk_040HeX1t2aOrLjOOjx2LlCGVCjdJwaQUJ_9rCpI4vYCFDaM2sn3fPb'   // あなたのPayPal Client Secret
 );
 const client = new paypal.core.PayPalHttpClient(environment);
 
